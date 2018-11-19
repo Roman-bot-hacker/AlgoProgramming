92f8010d8caef1c95fcebe8da787ebf583658695
Roman Kruk <roman1writer@gmail.com>
2018-11-19 09:30:49 +0200

    method's and variable's names are fixed

--- lab5/electro_manager.py
+++ lab5/electro_manager.py
@@ -30,27 +30,27 @@ max_heights = [5,12,48,62,71,1,24,78,100,22,24]
 
 
 def find_max_length(v_distance, v_max_heights):
-    return round(max(__recursive_fun(len(max_heights) - 2, 0, 1)[0],
-                     __recursive_fun(len(max_heights) - 2, 0, max_heights[-1])[0]), 2)
+    return round(max(__find_wires_length(len(max_heights) - 2, 0, 1)[0],
+                     __find_wires_length(len(max_heights) - 2, 0, max_heights[-1])[0]), 2)
 
 
 @memoized
-def __recursive_fun(step, wires_len, previous_height):
+def __find_wires_length(step, wires_len, previous_height):
     if step > 0:
-        rec1 = __recursive_fun(step - 1, __get_length(distance, previous_height, 1) + wires_len, 1)
-        rec2 = __recursive_fun(step - 1, __get_length(distance, previous_height, max_heights[step]) + wires_len, max_heights[step])
+        height_min = __find_wires_length(step - 1, __length_between_pillars(distance, previous_height, 1) + wires_len, 1)
+        height_max = __find_wires_length(step - 1, __length_between_pillars(distance, previous_height, max_heights[step]) + wires_len, max_heights[step])
     if step == 0:
-        rec1 = (__get_length(distance, previous_height, 1) + wires_len, 1)
-        rec2 = (__get_length(distance, previous_height, max_heights[step]) + wires_len, max_heights[step])
-    if rec1[0] > rec2[0]:
-        wires_len = rec1[0]
-        current_height = rec1[1]
+        height_min = (__length_between_pillars(distance, previous_height, 1) + wires_len, 1)
+        height_max = (__length_between_pillars(distance, previous_height, max_heights[step]) + wires_len, max_heights[step])
+    if height_min[0] > height_max[0]:
+        wires_len = height_min[0]
+        current_height = height_min[1]
     else:
-        wires_len = rec2[0]
-        current_height = rec1[1]
+        wires_len = height_max[0]
+        current_height = height_min[1]
     return wires_len, current_height
 
 
-def __get_length(distance, h1, h2):
+def __length_between_pillars(distance, h1, h2):
     h = h2 - h1
     return sqrt(h ** 2 + distance ** 2)
